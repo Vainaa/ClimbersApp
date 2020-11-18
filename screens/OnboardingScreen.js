27c7d8a9cc77de9e7fed3f6cb4eba2c16d41f638
Ilmari <ilmari.annila@student.laurea.fi>
2020-11-18 16:14:12 +0200

    first commit

---
+++ screens/OnboardingScreen.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { View, Text, Button, StyleSheet, Image } from 'react-native';
+import  Onboarding from 'react-native-onboarding-swiper';
+
+
+const OnboardingScreen = ({navigation}) => {
+    return (
+        <Onboarding
+        
+        onSkip={() => navigation.replace("Login")}
+        onDone={() => navigation.navigate("Login")}
+        pages={[
+            {
+                backgroundColor: "#fff",
+                image: <Image source={require('../assets/onboarding-img1.jpg')}/>,
+                title: "Onboarding 1",
+                subtitle: "Done with react native onboarding swiper",
+            },
+            {
+                backgroundColor: "#fff",
+                image: <Image source={require('../assets/onboarding-img2.jpg')}/>,
+                title: "Onboarding 2",
+                subtitle: "Done with react native onboarding swiper"
+            },
+            {
+                backgroundColor: "#fff",
+                image: <Image source={require('../assets/onboarding-img3.jpg')}/>,
+                title: "Onboarding 3",
+                subtitle: "Done with react native onboarding swiper"
+            },
+            
+        ]}
+        />
+    );
+};
+
+export default OnboardingScreen;
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        alignItems: "center",
+        justifyContent: "center"
+    },
+});
